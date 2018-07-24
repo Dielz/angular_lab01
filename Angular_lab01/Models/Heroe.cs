25e09f1a9a8431da6abc6e30d5f89d5b9f24b416
Dielz <Darkfalz674@gmail.com>
2018-07-24 10:54:53 -0400

    Add project files.

---
+++ Angular_lab01/Models/Heroe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular_lab01.Models
+{
+    public class Heroe
+    {
+        public int id { get; set; }
+        public string name{ get; set; }
+
+    }
+}
---
+++ Angular_lab01/Models/HeroesService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular_lab01.Models
+{
+    public class HeroesService
+    {
+
+
+       public IEnumerable<Heroe> GetHeroes()
+        {
+            return new List<Heroe>() {
+              new Heroe { id= 11, name= "Mr. Nice" },
+              new Heroe { id= 12, name= "Narco" },
+              new Heroe { id= 13, name= "Bombasto" },
+              new Heroe { id= 14, name= "Celeritas" },
+              new Heroe { id= 15, name= "Magneta" },
+              new Heroe { id= 16, name= "RubberMan" },
+              new Heroe { id= 17, name= "Dynama" },
+              new Heroe { id= 18, name= "Dr IQ" },
+              new Heroe { id= 19, name= "Magma" },
+              new Heroe { id= 20, name= "Tornado" }
+
+            };
+        }
+
+    }
+}
