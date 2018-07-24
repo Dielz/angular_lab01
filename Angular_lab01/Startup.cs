ab595deaf8394557ae7ae43ead1ea58759f1faf4
Dielz <Darkfalz674@gmail.com>
2018-07-24 11:08:34 -0400

    test2

--- Angular_lab01/Startup.cs
+++ Angular_lab01/Startup.cs
@@ -18,11 +18,13 @@ namespace Angular_lab01
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+            //empty comment
         }
 
         public IConfiguration Configuration { get; } 
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
